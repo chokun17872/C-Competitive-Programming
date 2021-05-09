35716a0f286133a1470cccabf9ba3935cb7e31ca
Chavalvit Keartnattakorn <83200507+chokun17872@users.noreply.github.com>
2021-05-09 22:04:15 +0700

    Add files via upload

---
+++ POSN/Camp2/Stair.cpp
@@ -0,0 +1,25 @@
+/*
+TASK: Stair
+AUTHOR: passa
+LANG: C++
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int a[5101]={1};
+int main(){
+
+	int i,j,n,k;
+	scanf("%d %d",&n,&k);
+	for(i=1 ; i<=n ; i++){
+		for(j=n ; j>=i ; j--){
+			a[j]+=a[j-i];
+			a[j]%=k;
+		}
+	}
+	printf("%d\n",a[n]-1);
+	return 0;
+}
+/*
+a[j-i] describe split out
+*/
+
